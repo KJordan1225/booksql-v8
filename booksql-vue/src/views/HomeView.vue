a0ee2252ce2c3d3a1541272bbe7145751dbc3322
KJordan1225 <102583256+KJordan1225@users.noreply.github.com>
2022-07-21 15:39:17 -0400

    added vue apollo and sample query - not working

--- booksql-vue/src/views/HomeView.vue
+++ booksql-vue/src/views/HomeView.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <div v-for="category in categories" :key="category.id">
+      {{category.namr}}
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+import gql from 'graphql-tag'
 
 export default {
   name: 'HomeView',
+  apollo: {
+    categories: gql`query {
+      categories {
+        id
+        name
+      }
+    }`,
+  },
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
