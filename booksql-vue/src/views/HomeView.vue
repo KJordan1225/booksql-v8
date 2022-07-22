3992391a738d7970e78565bd397f67afe993be7e
KJordan1225 <102583256+KJordan1225@users.noreply.github.com>
2022-07-22 12:07:39 -0400

    completed first ApolloQuery in component - results displayed successfully

--- booksql-vue/src/views/HomeView.vue
+++ booksql-vue/src/views/HomeView.vue
@@ -1,10 +1,37 @@
 <template>
   <div class="home">
     <!-- <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <div v-for="category in categories" :key="category.id">
-      {{category.namr}}
-    </div>
+    <HelloWorld msg="Welcome to Your Vue.js App"/> --> 
+    <ApolloQuery
+      :query="gql => gql`
+        query {
+        categories {
+          id
+          name
+        }
+      }
+      `"
+      :variables="{ id, name }"
+    >
+      <template v-slot="{ result: { loading, error, data } }">
+        <!-- Loading -->
+        <div v-if="loading" class="loading apollo">Loading...</div>
+
+        <!-- Error -->
+        <div v-else-if="error" class="error apollo">An error occurred</div>
+
+        <!-- Result -->
+        <div v-else-if="data" class="category" >
+          <ul>
+            <li v-for="category in categories" :key="category.id" class="category">
+              {{ category.id }} {{ category.name }}
+            </li>
+          </ul>
+        </div>
+        <!-- No result -->
+        <div v-else class="no-result apollo">No result :(</div>
+      </template>
+    </ApolloQuery>
   </div>
 </template>
 
@@ -15,6 +42,11 @@ import gql from 'graphql-tag'
 
 export default {
   name: 'HomeView',
+  data() {
+    return {
+      categories: []
+    }
+  },
   apollo: {
     categories: gql`query {
       categories {
